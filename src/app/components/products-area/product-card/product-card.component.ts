8994d94f43c0085e0669cf065ac79533a567ebe1
Shon Benayoun <shonba98@gmail.com>
2024-01-14 15:39:07 +0200

    addedDeleteProduct

--- src/app/components/products-area/product-card/product-card.component.ts
+++ src/app/components/products-area/product-card/product-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import ProductModel from '../../../models/product-model';
+import { ProductsService } from '../../../services/products.service';
 
 @Component({
   selector: 'app-product-card',
@@ -9,8 +10,15 @@ import ProductModel from '../../../models/product-model';
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
-  
+
+  public constructor(private productsService: ProductsService) { }
   @Input()
   public product: ProductModel; // Like props in react.
-  
+
+  public async deleteMe(id: number) {
+    try {
+      await this.productsService.deleteProduct(id);
+    } catch (err: any) { alert(err) };
+
+  }
 }
--- src/app/components/products-area/products-list/products-list.component.ts
+++ src/app/components/products-area/products-list/products-list.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import ProductModel from '../../../models/product-model';
-import { ProductCardComponent } from '../product-card/product-card.component';
-import { ProductsService } from '../../../services/products.service';
 import { RouterLink } from '@angular/router';
+import ProductModel from '../../../models/product-model';
+import { ProductsService } from '../../../services/products.service';
+import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
   selector: 'app-products-list',
@@ -13,19 +13,19 @@ import { RouterLink } from '@angular/router';
   templateUrl: './products-list.component.html',
   styleUrl: './products-list.component.css'
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent implements OnInit{
   public products: ProductModel[];
 
-  public constructor(private title: Title, private productsService: ProductsService) { }
+  public constructor(private title: Title, private productsService:ProductsService) { }
 
   public async ngOnInit() {
-    try {
-      this.title.setTitle("Product List");
-      this.products = await this.productsService.getAllProducts();
-    }
-    catch (err: any) {
-      alert(err.message);
-    }
+      try {
+          this.title.setTitle("Product List");
+          this.products = await this.productsService.getAllProducts();
+      }
+      catch (err: any) {
+          alert(err.message);
+      }
   }
 
 }
